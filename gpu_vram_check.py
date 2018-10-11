2cbf8feca9cc2364209b2aaf9db690e4536a300f
Johannes Bandgren <johannes.bandgren@altran.com>
2018-10-11 15:05:06 +0200

    test

--- gpu_vram_check.py
+++ gpu_vram_check.py
@@ -2,6 +2,15 @@ import os
 
 import face_recognition
 
-path = 
+path = "/home/dev/GIT/facial_be/pictures"
 
-for root, dir, files in os.walk():
+for root, dir, files in os.walk(path):
+    for file in files:
+        images = []
+        for i in range(10):
+            images.append(face_recognition.load_image_file(file))
+        print("Starting facelocation:")
+        for image in images:
+            face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+
+
