57d7c16b0f6e8d3b172d2f5bfa1a81e358594f18
Johannes Bandgren <johannes.bandgren@altran.com>
2018-10-11 15:07:13 +0200

    test

--- gpu_vram_check.py
+++ gpu_vram_check.py
@@ -8,7 +8,7 @@ for root, dir, files in os.walk(path):
     for file in files:
         images = []
         for i in range(20):
-            images.append(face_recognition.load_image_file(file))
+            images.append(face_recognition.load_image_file(path + "/" + file))
         print("Starting facelocation for" + str(file))
         for image in images:
             face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
